ad0f2baa3ca1d11301a022348b0bf25085a9a3fb
dnkntksh <mr.kaz71@gmail.com>
2017-09-12 23:30:13 +0900

    セレクトボックス、チェックボックスの選択肢の修正

--- src/main/java/app/echo/EchoController.java
+++ src/main/java/app/echo/EchoController.java
@@ -14,13 +14,37 @@ import org.springframework.web.bind.annotation.*;
 @Controller
 @RequestMapping("echo")
 public class EchoController {
+	/**
+	 * リストボックスの選択肢
+	 */
+	final static List<SelectTestBean> SELECT_ITEMS =
+		    new ArrayList<SelectTestBean>() {
+		    {
+		    	add(new SelectTestBean(0, ""));
+				add(new SelectTestBean(1, "test"));
+				add(new SelectTestBean(2, "テスト"));
+				add(new SelectTestBean(3, "てすと"));
+		    }
+	};
+	
+	/**
+	 * チェックボックスの選択肢
+	 */
+	final static List<CheckBoxesTestBean> SELECT_CHECKBOX_ITEMS =
+		    new ArrayList<CheckBoxesTestBean>() {
+		    {
+		    	add(new CheckBoxesTestBean(123, "Spring"));
+				add(new CheckBoxesTestBean(456, "Framework"));
+				add(new CheckBoxesTestBean(789, "MVC"));		
+		    }
+	};
 	
 	@RequestMapping(method = RequestMethod.GET)
 	public String viewInput(Model model){
 		// 初期値設定
 		EchoForm form = new EchoForm();
-		this.settingSelectBox(form);
-		this.settingCheckBoxes(form);
+		form.setSelectTestList(SELECT_ITEMS);
+		form.setCheckBoxesList(SELECT_CHECKBOX_ITEMS);
 		model.addAttribute(form);
 		return "echo/input";
 	}
@@ -29,33 +53,10 @@ public class EchoController {
 	public String echo(@Valid EchoForm form, BindingResult result){
 		// 入力チェック
 		if(result.hasErrors()){
-			settingSelectBox(form);
-			this.settingCheckBoxes(form);
+			form.setSelectTestList(SELECT_ITEMS);
+			form.setCheckBoxesList(SELECT_CHECKBOX_ITEMS);
 			return "echo/input";
 		}
 		return "echo/output";
-	}
-	
-	/**
-	 * コンボボックスの初期設定
-	 * @param model
-	 */
-	private void settingSelectBox(EchoForm form){
-		List<SelectTestBean> selectBoxList = new ArrayList<SelectTestBean>();
-				
-		selectBoxList.add(new SelectTestBean(0, ""));
-		selectBoxList.add(new SelectTestBean(1, "test"));
-		selectBoxList.add(new SelectTestBean(2, "テスト"));
-		selectBoxList.add(new SelectTestBean(3, "てすと"));		
-		form.setSelectTestList(selectBoxList);
-	}
-	
-	private void settingCheckBoxes(EchoForm form){
-		List<CheckBoxesTestBean> selectTestList = new ArrayList<CheckBoxesTestBean>();
-		selectTestList.add(new CheckBoxesTestBean(123, "Spring"));
-		selectTestList.add(new CheckBoxesTestBean(456, "Framework"));
-		selectTestList.add(new CheckBoxesTestBean(789, "MVC"));		
-		form.setCheckBoxesList(selectTestList);
-		
 	}
 }
